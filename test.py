40b68e0250e63de072d75147673d706153547554
buddapallavi127 <buddapallavi057@gmail.com>
2025-05-17 12:13:28 +0530

    Remove test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyDBtabtbDOvtyANPPa2hNiHNv0aUrZKxNk")
-
-for model in genai.list_models():
-    print(model.name, model.supported_generation_methods)
